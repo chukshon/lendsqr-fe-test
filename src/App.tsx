067f4b8cac6eeb4f165a74b512e226285cd14375
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-10-07 15:27:09 +0100

    create the route structure in the app tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 function App() {
   return (
     <div className="App">
@@ -15,7 +17,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
