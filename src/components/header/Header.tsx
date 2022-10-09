c1d5ac59fd9afeaa78cd345fde7b74d946b98954
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-10-09 07:12:33 +0100

    dashboard header started

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,9 @@
 import React from "react"
+import styles from "./header.module.scss"
+import { HiOutlineMenuAlt2 } from "react-icons/hi"
 
 const Header = () => {
-  return <div>Header</div>
+  return <div className={styles.header__container}></div>
 }
 
 export default Header
