a66c9731082f5b415f9e488f30ef9a4dd91716ba
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-10-09 08:07:44 +0100

    search  bar done

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 import styles from "./searchBar.module.scss"
+import { AiOutlineSearch } from "react-icons/ai"
 const SearchBar = () => {
   return (
     <div className={styles.searchbar__container}>
-      <input type="text" placeholder="Search for anything" />
-      <div className="search__container"></div>
+      <input
+        type="text"
+        placeholder="Search for anything"
+        className={styles.input}
+      />
+      <div className={styles.search__icon}>
+        <AiOutlineSearch />
+      </div>
     </div>
   )
 }
