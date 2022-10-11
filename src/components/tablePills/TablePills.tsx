8450a6f663efb17285387713e934907e10ba7e9f
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-10-11 11:27:26 +0100

    user page started

--- src/components/tablePills/TablePills.tsx
+++ src/components/tablePills/TablePills.tsx
@@ -5,8 +5,10 @@ import styles from "./tablepills.module.scss"
 import { FiUserX } from "react-icons/fi"
 import { BiUserCheck } from "react-icons/bi"
 import { BsEye } from "react-icons/bs"
+import { useNavigate } from "react-router-dom"
 
 const TablePills = () => {
+  const navigate = useNavigate()
   const [filterOpen, setFilterOpen] = React.useState<boolean>(false)
   const handleClickAway = () => {
     setFilterOpen(false)
@@ -29,7 +31,11 @@ const TablePills = () => {
         </div>
         {filterOpen && (
           <ul className={styles.actions__container}>
-            <li>
+            <li
+              onClick={() => {
+                navigate("/user-details")
+              }}
+            >
               <BsEye />
               <p>View Details</p>
             </li>
--- src/pages/dashboard/userDetails/UserDetails.tsx
+++ src/pages/dashboard/userDetails/UserDetails.tsx
@@ -1,7 +1,26 @@
 import React from "react"
+import styles from "./userDetails.module.scss"
+import { CgArrowLongLeft } from "react-icons/cg"
+import Button from "../../../components/button/Button"
 
 const UserDetails = () => {
-  return <div>User Details</div>
+  return (
+    <div className={styles.wrapper}>
+      <button className={styles.back__btn}>
+        <span className={styles.back__btn__icon}>
+          <CgArrowLongLeft size={"20px"} />
+        </span>
+        <span className={styles.back__btn__text}>Back to Users</span>
+      </button>
+      <div className={styles.top__container}>
+        <h3>User Details</h3>
+        <div className={styles.button__group}>
+          <Button buttonType={"red"} buttonText={"BLACKLIST USER"} />
+          <Button buttonType={"green"} buttonText={"ACTIVATE USER"} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default UserDetails
